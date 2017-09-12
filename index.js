6c180b4ee9bca30e5e3bbd9dfc632b6d6a741c45
Zach Siegel <zachsiegel41@gmail.com>
2017-09-12 19:01:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-function add(x,y) {
-  return (x+y)
+function add (x,y) {
+  return x+y
 }
 
-function subtract(x,y) {
-  return (x-y)
+function subtract (x,y) {
+  return x-y
 }
 
-function multiply(x,y) {
-  return (x*y)
+function multiply (x,y) {
+  return x*y
 }
 
-function divide(x,y) {
-  return (x/y)
+function divide (x,y) {
+  return x/y
 }
+
