329b449ad2f64ad43f20b94f526cba7a9db78eef
Zach Siegel <zachsiegel41@gmail.com>
2017-09-12 19:05:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-function add (x,y) {
-  return x+y
-}
 
-function subtract (x,y) {
-  return x-y
-}
-
-function multiply (x,y) {
-  return x*y
-}
-
-function divide (x,y) {
-  return x/y
-}
 
 function inc(n) {
   return n+=
